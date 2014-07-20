2ad92760798caee2c2b6c27a4425d550739b2720
Yves Stadler <mvyonline@gmail.com>
2014-07-20 21:18:02 +0200

    Breadth First Search implementation for Dijkstra

--- robot.js
+++ robot.js
@@ -45,11 +45,111 @@
  * - le robot aura consommé 1 mL de carburant
  */
 
+/*
+
+Queue.js
+
+A function to represent a queue
+
+Created by Stephen Morley - http://code.stephenmorley.org/ - and released under
+the terms of the CC0 1.0 Universal legal code:
+
+http://creativecommons.org/publicdomain/zero/1.0/legalcode
+
+*/
+
+/* Creates a new queue. A queue is a first-in-first-out (FIFO) data structure -
+ * items are added to the end of the queue and removed from the front.
+ */
+function Queue(){
+
+  // initialise the queue and offset
+  var queue  = [];
+  var offset = 0;
+
+  // Returns the length of the queue.
+  this.getLength = function(){
+    return (queue.length - offset);
+  }
+
+  // Returns true if the queue is empty, and false otherwise.
+  this.isEmpty = function(){
+    return (queue.length == 0);
+  }
+
+  /* Enqueues the specified item. The parameter is:
+   *
+   * item - the item to enqueue
+   */
+  this.enqueue = function(item){
+    queue.push(item);
+  }
+
+  /* Dequeues an item and returns it. If the queue is empty, the value
+   * 'undefined' is returned.
+   */
+  this.dequeue = function(){
+
+    // if the queue is empty, return immediately
+    if (queue.length == 0) return undefined;
+
+    // store the item at the front of the queue
+    var item = queue[offset];
+
+    // increment the offset and remove the free space if necessary
+    if (++ offset * 2 >= queue.length){
+      queue  = queue.slice(offset);
+      offset = 0;
+    }
+
+    // return the dequeued item
+    return item;
+
+  }
+
+  /* Returns the item at the front of the queue (without dequeuing it). If the
+   * queue is empty then undefined is returned.
+   */
+  this.peek = function(){
+    return (queue.length > 0 ? queue[offset] : undefined);
+  }
+
+  /* Returns the index of an element */
+  this.indexOf = function(lookup) {
+      var idx = queue.indexOf(lookup, offset) - offset;
+      return (idx < 0 ? -1 : idx);
+  }
+
+  this.get = function(index) {
+      return queue[index + offset];
+  }
+
+  this.put = function(index, val) {
+      queue[index + offset] = val;
+  }
+
+  this.reset = function() {
+      while(!this.isEmpty()) {
+          this.dequeue();
+      }
+  }
+
+  this.log = function() {
+      console.log('length = ' + this.getLength() +
+              ' offset = ' + offset +
+              ' queue = ' + queue);
+  }
+}
+/* END Queue.js */
+
 
 // pcc = plus court chemin
 // tableau dijkstra
 var pcc = new Array();
+var pcc_mark = new Array();
 var liste_stations = new Array();
+var cell_queue = new Queue();
+var cell_exists = new Queue();
 
 function init_custom() {
 	for (j = hauteur_terrain; j >= 1; j--) {
@@ -60,6 +160,24 @@ function init_custom() {
 	}
 	debug_pcc();
 }
+
+function init_mark() {
+    for(j = 1; j <= hauteur_terrain; j++) {
+        pcc_mark[j] = new Array();
+		for (i = 1; i <= largeur_terrain; i++) {
+			pcc_mark[j][i] = false;
+		}
+    }
+}
+
+function reset_mark() {
+    for(j = 1; j <= hauteur_terrain; j++) {
+		for (i = 1; i <= largeur_terrain; i++) {
+			pcc_mark[j][i] = false;
+		}
+    }
+}
+
 function debug_pcc() {
 	for (j = hauteur_terrain; j >= 1; j--) {
 		for (i = 1; i <= largeur_terrain; i++) {
@@ -81,51 +199,11 @@ function debug_pcc() {
 		}
 	}
 }
+
 init_custom();
+init_mark();
 
-function dijkstra_maj_cell_recursive(y, x, dist, toLeft, toRight, toTop, toBottom, onlyUnknown)
-{
-	// OutOfBounds ou déjà plus court
-	if (y <= 0 || y > hauteur_terrain || x <= 0 || x > largeur_terrain) {
-		return;
-	}
-	if (onlyUnknown && dist > 0 && pcc[y][x] < 99) {
-		return;
-	}
-	var cell = terrain_explore[y][x];
-	// 1 = obstacle, 9 == inexploré
-	if (cell == 1 || cell == 9) {
-		return;
-	}
-
-	count_recursive++;
-
-	if (dist < pcc[y][x]) { 
-		pcc[y][x] = dist;
-	} else {
-		dist = pcc[y][x];
-	}
-
-	// Récursion
-	if (toTop && typeof pcc[y + 1] != 'undefined') {
-		dijkstra_maj_cell_recursive(y + 1, x, dist + 1, toLeft, toRight, toTop, false, onlyUnknown);
-	}
-	if (toBottom && typeof pcc[y - 1] != 'undefined') {
-		dijkstra_maj_cell_recursive(y - 1, x, dist + 1, toLeft, toRight, false, toBottom, onlyUnknown);
-	}
-	if (toLeft && typeof pcc[y][x - 1] != 'undefined') {
-		dijkstra_maj_cell_recursive(y, x - 1, dist + 1, toLeft, false, toTop, toBottom, onlyUnknown);
-	}
-	if (toRight && typeof pcc[y][x + 1] != 'undefined') {
-		dijkstra_maj_cell_recursive(y, x + 1, dist + 1, false, toRight, toTop, toBottom, onlyUnknown);
-	}
-}
-
-function dijkstra_maj_unknown_cell(y, x) {
-	if (pcc[y][x] < 99) {
-		//console.log('skip', y, x, pcc[y][x]);
-		//return false;
-	}
+function dijkstra_bfs_maj_ukn(y, x) {
 	// Recherche du minimal adjacent
 	var min = [pcc[y][x]];
 	if (y > 1) {
@@ -141,15 +219,94 @@ function dijkstra_maj_unknown_cell(y, x) {
 		min.push(pcc[y][x + 1]);
 	}
 	min = Math.min.apply(null, min);
-	console.log(y, x, "=", min);
-	dijkstra_maj_cell_recursive(y, x, min + 1, true, true, true, true);
+	//console.log(y, x, "=", min);
+	dijkstra_bfs_maj(y, x, min + 1, true);
 	return true;
+}
+
+
+
+/* Ajoute une case à la file d'attente */
+function addToQueue(y, x, dist) {
+    /* If node has not already been visited */
+    if(pcc_mark[y][x] == false ) {
+        /* If it is not already on the list */
+        var cell = cell_exists.indexOf(y + '_' + x);
+
+        if(cell == -1) {
+            //console.log('Adding to queue ' + y + '/' + x);
+            cell_exists.enqueue(y + '_' + x);
+            cell_queue.enqueue([y, x, dist]);
+        } else {
+            var  val = cell_queue.get(cell);
+            if(val[2] > dist) {
+                //console.log('Updating in queue ' + y + '/' + x);
+                val[2] = dist;
+                cell_queue.put(cell, val);
+            }
+        }
+    }
+}
+
+/* BFS Dijkstra */
+function dijkstra_bfs_maj(y, x, dist, onlyUnknown) {
+    /* Reset the variables */
+    cell_queue.reset();
+    cell_exists.reset();
+    reset_mark();
+
+    /* Ajoute la première case à la file */
+    addToQueue(y, x, dist);
+
+    while(!cell_queue.isEmpty()) {
+        //console.log(cell_queue.getLength());
+        /* The working node */
+        //cell_queue.log();
+        var step = cell_queue.dequeue();
+        cell_exists.dequeue();
+        //cell_queue.log();
+
+        /* The node is updated by the value */
+        var texp = terrain_explore[step[0]][step[1]];
+        if(texp != 1 && texp != 9) {
+            /* If no value in cell */
+            if((pcc[step[0]][step[1]] < 99 /*&& !onlyUnknown*/) 
+                    || (pcc[step[0]][step[1]] == 99)) {
+                /* If dist is lower */
+                console.log('Updating ' + step[0] + '/' + step[1] + ' to ' + step[2] + ' from ' + pcc[step[0]][step[1]] + ' to ' + step[2]);
+                if(pcc[step[0]][step[1]] > step[2]) {
+                    pcc[step[0]][step[1]] = step[2];
+                }
+            }
+        }
+
+        pcc_mark[step[0]][step[1]] = true;
+
+        /* Adding all the neighbour to the queue */
+        var x = step[1];
+        var y = step[0];
+        var d = step[2] + 1;
+
+        if(typeof pcc[y + 1] != 'undefined') {
+            addToQueue(y + 1, x, d);
+        }
+        if(typeof pcc[y - 1] != 'undefined') {
+            addToQueue(y - 1, x, d);
+        }
+        if(typeof pcc[y][x + 1] != 'undefined') {
+            addToQueue(y, x + 1, d);
+        }
+        if(typeof pcc[y][x - 1] != 'undefined') {
+            addToQueue(y, x - 1, d);
+        }
+    }
 }
 
 function ajoute_station(j, i) {
 	if (typeof liste_stations[j + '_' + i] == 'undefined') {
 		liste_stations[j + '_' + i] = [j, i];
-		dijkstra_maj_cell_recursive(j, i, 0, true, true, true, true);
+		//dijkstra_maj_cell_recursive(j, i, 0, true, true, true, true);
+        dijkstra_bfs_maj(j, i, 0);
 	}
 }
 
@@ -157,10 +314,10 @@ function decider_direction(CH, CB, CG, CD, but_dir_hb, but_dir_gd, but_dist, res
 
 	count_recursive = 0;
 	for (station in liste_stations) {
-		dijkstra_maj_cell_recursive(liste_stations[station][0], liste_stations[station][1], 0, true, true, true, true, true);
+		dijkstra_bfs_maj(liste_stations[station][0], liste_stations[station][1], 0, true);
 	}
 	
-	console.log('maj station', count_recursive);
+	//console.log('maj station', count_recursive);
 	// On voit une station
 	if (CH.voit == 3) {
 		ajoute_station(robot_y + CH.dist, robot_x);
@@ -174,11 +331,13 @@ function decider_direction(CH, CB, CG, CD, but_dir_hb, but_dir_gd, but_dist, res
 	if (CG.voit == 3) {
 		ajoute_station(robot_y, robot_x - CG.dist);
 	}
-	console.log('add station', count_recursive);
+	//console.log('add station', count_recursive);
 
 	// Mise à jour case courante
-	dijkstra_maj_unknown_cell(robot_y, robot_x);
-	console.log('robot', count_recursive);
+	//dijkstra_maj_unknown_cell(robot_y, robot_x);
+    dijkstra_bfs_maj_ukn(robot_y, robot_x);
+    
+	//console.log('robot', count_recursive);
 
 	debug_pcc();
 
