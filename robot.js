720743d9f969f82f3605f8ac7c84f0bf5080c828
Yves Stadler <mvyonline@gmail.com>
2014-07-22 20:24:01 +0200

    Adding BFS Dijkstra for goal

--- robot.js
+++ robot.js
@@ -52,6 +52,7 @@
 // tableau dijkstra
 var pcc = new Array();
 var pcc_mark = new Array();
+var pcc_goal = new Array();
 var astar_map = new Array();
 var astar_weight = new Array();
 var liste_stations = new Array();
@@ -59,32 +60,28 @@ var cell_queue = new Queue();
 var cell_exists = new Queue();
 
 function init_custom() {
-	for (j = hauteur_terrain; j >= 1; j--) {
-		pcc[j] = new Array();
-		astar_weight[j - 1] = new Array();
-		for (i = 1; i <= largeur_terrain; i++) {
-			pcc[j][i] = 99;
-			astar_weight[j - 1][i - 1] = 1;
-		}
-	}
-	debug_pcc();
+    for (j = hauteur_terrain; j >= 1; j--) {
+        pcc[j] = new Array();
+        pcc_goal[j] = new Array();
+        astar_weight[j - 1] = new Array();
+        pcc_mark[j] = new Array();
+        for (i = 1; i <= largeur_terrain; i++) {
+            pcc[j][i] = 99;
+            pcc_goal[j][i] = 99;
+            astar_weight[j - 1][i - 1] = 1;
+            pcc_mark[j][i] = false;
+        }
+    }
+    debug_pcc();
+    debug_pcc_goal();
 }
 
-function init_mark() {
-	for(j = 1; j <= hauteur_terrain; j++) {
-		pcc_mark[j] = new Array();
-		for (i = 1; i <= largeur_terrain; i++) {
-			pcc_mark[j][i] = false;
-		}
-	}
-}
-
-function reset_mark() {
-	for(j = 1; j <= hauteur_terrain; j++) {
-		for (i = 1; i <= largeur_terrain; i++) {
-			pcc_mark[j][i] = false;
-		}
-	}
+function bfs_reset_mark() {
+    for(j = 1; j <= hauteur_terrain; j++) {
+        for (i = 1; i <= largeur_terrain; i++) {
+            pcc_mark[j][i] = false;
+        }
+    }
 }
 
 /* Debug procedures */
@@ -110,6 +107,28 @@ function debug_pcc() {
 	}
 }
 
+function debug_pcc_goal() {
+	for (j = hauteur_terrain; j >= 1; j--) {
+		for (i = 1; i <= largeur_terrain; i++) {
+			var str = '';
+			if (j == robot_y && i == robot_x) {
+				str += '<strong>';
+			}
+			if (terrain_explore[j][i] == 9) {
+				str += '<img src="img/inconnu.gif" />';
+			} else if (terrain_explore[j][i] == 1) {
+				str += '<img src="img/obstacle.gif" />';
+			} else {
+				str += pcc_goal[j][i] == 99 ? '.' : pcc_goal[j][i];
+			}
+			if (j == robot_y && i == robot_x) {
+				str += '</strong>';
+			}
+			document.getElementById('pcc_goal_' + j + '_' + i).innerHTML = str;
+		}
+	}
+}
+
 function debug_astar(chemin) {
 	for (j = 0; j < hauteur_terrain; j++) {
 		for (i = 0; i < largeur_terrain; i++) {
@@ -129,32 +148,31 @@ function debug_astar(chemin) {
 /* End of debug */
 
 init_custom();
-init_mark();
 
-function dijkstra_bfs_maj_ukn(y, x) {
+function dijkstra_bfs_maj_ukn(tab, y, x) {
 	// Recherche du minimal adjacent
-	var min = [pcc[y][x]];
+	var min = [tab[y][x]];
 	if (y > 1) {
-		min.push(pcc[y - 1][x]);
+		min.push(tab[y - 1][x]);
 	}
 	if (y < hauteur_terrain) {
-		min.push(pcc[y + 1][x]);
+		min.push(tab[y + 1][x]);
 	}
 	if (x > 1) {
-		min.push(pcc[y][x - 1]);
+		min.push(tab[y][x - 1]);
 	}
 	if (x < largeur_terrain) {
-		min.push(pcc[y][x + 1]);
+		min.push(tab[y][x + 1]);
 	}
 	min = Math.min.apply(null, min);
-	dijkstra_bfs_maj(y, x, min + 1, true);
+	dijkstra_bfs_maj(tab, y, x, min + 1, true);
 	return true;
 }
 
 
 
 /* Ajoute une case à la file d'attente */
-function addToQueue(y, x, dist) {
+function bfs_addToQueue(y, x, dist) {
     /* If node has not already been visited */
     if(pcc_mark[y][x] == false ) {
         if(terrain_explore[y][x] != 1 && terrain_explore[y][x] != 9) {
@@ -176,14 +194,14 @@ function addToQueue(y, x, dist) {
 }
 
 /* BFS Dijkstra */
-function dijkstra_bfs_maj(y, x, dist, onlyUnknown) {
+function dijkstra_bfs_maj(tab, y, x, dist, onlyUnknown) {
     /* Reset the variables */
     cell_queue.reset();
     cell_exists.reset();
-    reset_mark();
+    bfs_reset_mark();
 
     /* Ajoute la première case à la file */
-    addToQueue(y, x, dist);
+    bfs_addToQueue(y, x, dist);
 
     while(!cell_queue.isEmpty()) {
         /* The working node */
@@ -191,11 +209,11 @@ function dijkstra_bfs_maj(y, x, dist, onlyUnknown) {
         cell_exists.dequeue();
 
         /* If no value in cell */
-        if((pcc[step[0]][step[1]] < 99 /*&& !onlyUnknown*/) 
-            || (pcc[step[0]][step[1]] == 99)) {
+        if((tab[step[0]][step[1]] < 99 /*&& !onlyUnknown*/) 
+            || (tab[step[0]][step[1]] == 99)) {
             /* If dist is lower */
-            if(pcc[step[0]][step[1]] > step[2]) {
-                pcc[step[0]][step[1]] = step[2];
+            if(tab[step[0]][step[1]] > step[2]) {
+                tab[step[0]][step[1]] = step[2];
             }
         }
 
@@ -206,28 +224,27 @@ function dijkstra_bfs_maj(y, x, dist, onlyUnknown) {
         var y = step[0];
         var d = step[2] + 1;
 
-        if(typeof pcc[y + 1] != 'undefined') {
-            addToQueue(y + 1, x, d);
+        if(typeof tab[y + 1] != 'undefined') {
+            bfs_addToQueue(y + 1, x, d);
         }
-        if(typeof pcc[y - 1] != 'undefined') {
-            addToQueue(y - 1, x, d);
+        if(typeof tab[y - 1] != 'undefined') {
+            bfs_addToQueue(y - 1, x, d);
         }
-        if(typeof pcc[y][x + 1] != 'undefined') {
-            addToQueue(y, x + 1, d);
+        if(typeof tab[y][x + 1] != 'undefined') {
+            bfs_addToQueue(y, x + 1, d);
         }
-        if(typeof pcc[y][x - 1] != 'undefined') {
-            addToQueue(y, x - 1, d);
+        if(typeof tab[y][x - 1] != 'undefined') {
+            bfs_addToQueue(y, x - 1, d);
         }
     }
 }
 
 function ajoute_station(j, i) {
-	if (typeof liste_stations[j + '_' + i] == 'undefined') {
-		liste_stations[j + '_' + i] = [j, i];
-		//dijkstra_maj_cell_recursive(j, i, 0, true, true, true, true);
-		dijkstra_bfs_maj(j, i, 0);
-	}
-	return false;
+    if (typeof liste_stations[j + '_' + i] == 'undefined') {
+        liste_stations[j + '_' + i] = [j, i];
+        dijkstra_bfs_maj(j, i, 0);
+    }
+    return false;
 }
 
 function chemin_astar()
@@ -254,92 +271,94 @@ function chemin_astar()
 
 function decider_direction(CH, CB, CG, CD, but_dir_hb, but_dir_gd, but_dist, reserve_carburant) {
 
-	count_recursive = 0;
-	for (station in liste_stations) {
-		dijkstra_bfs_maj(liste_stations[station][0], liste_stations[station][1], 0, true);
-	}
-	
-	// On voit une station
-	if (CH.voit == 3) {
-		ajoute_station(robot_y + CH.dist, robot_x);
-	}
-	if (CB.voit == 3) {
-		ajoute_station(robot_y - CB.dist, robot_x);
-	}
-	if (CD.voit == 3) {
-		ajoute_station(robot_y, robot_x + CD.dist);
-	}
-	if (CG.voit == 3) {
-		ajoute_station(robot_y, robot_x - CG.dist);
-	}
+    count_recursive = 0;
+    for (station in liste_stations) {
+        dijkstra_bfs_maj(pcc, liste_stations[station][0], liste_stations[station][1], 0, true);
+    }
 
-	// Mise à jour case courante
-	//dijkstra_maj_unknown_cell(robot_y, robot_x);
-    dijkstra_bfs_maj_ukn(robot_y, robot_x);
-    
+    // On voit une station
+    if (CH.voit == 3) {
+        ajoute_station(robot_y + CH.dist, robot_x);
+    }
+    if (CB.voit == 3) {
+        ajoute_station(robot_y - CB.dist, robot_x);
+    }
+    if (CD.voit == 3) {
+        ajoute_station(robot_y, robot_x + CD.dist);
+    }
+    if (CG.voit == 3) {
+        ajoute_station(robot_y, robot_x - CG.dist);
+    }
 
-	debug_pcc();
+    // On Maj le but
+    dijkstra_bfs_maj(pcc_goal, but_y, but_x, 0);
 
-	// But visible et accessible => go
-	if (CH.voit==2 && CH.dist <= reserve_carburant) { return 'H'; }
-	if (CB.voit==2 && CB.dist <= reserve_carburant) { return 'B'; }
-	if (CG.voit==2 && CG.dist <= reserve_carburant) { return 'G'; }
-	if (CD.voit==2 && CD.dist <= reserve_carburant) { return 'D'; }
-	
-	
-	var pcc_robot = pcc[robot_y][robot_x];
-	if (reserve_carburant < 30 && pcc_robot >= reserve_carburant - 1) {
-		if (typeof pcc[robot_y + 1] != 'undefined' && pcc[robot_y + 1][robot_x] < pcc_robot) {
-			return "H";
-		}
-		if (typeof pcc[robot_y - 1] != 'undefined' && pcc[robot_y - 1][robot_x] < pcc_robot) {
-			return "B";
-		}
-		if (typeof pcc[robot_y][robot_x - 1] != 'undefined' && pcc[robot_y][robot_x - 1] < pcc_robot) {
-			return "G";
-		}
-		if (typeof pcc[robot_y][robot_x + 1] != 'undefined' && pcc[robot_y][robot_x + 1] < pcc_robot) {
-			return "D";
-		}
-	}
+    // Mise à jour case courante
+    dijkstra_bfs_maj_ukn(pcc, robot_y, robot_x);
 
-	// Une seule direction possible...
-	if (CD.dist + CB.dist + CG.dist == 3) { return 'H'; }
-	if (CH.dist + CG.dist + CD.dist == 3) { return 'B'; }
-	if (CH.dist + CD.dist + CB.dist == 3) { return 'G'; }
-	if (CH.dist + CG.dist + CB.dist == 3) { return 'D'; }
-	
-	var chemin = chemin_astar();
-	var prochain_x = chemin[0].y + 1;
-	var prochain_y = chemin[0].x + 1;
-	
-	debug_astar(chemin);
-		
-	if (prochain_x > robot_x) {
-		astar_weight[robot_y - 1][robot_x]++;
-		return "D";
-	}
-	if (prochain_x < robot_x) {
-		astar_weight[robot_y - 1][robot_x - 2]++;
-		return "G";
-	}
-	if (prochain_y > robot_y) {
-		astar_weight[robot_y][robot_x - 1]++;
-		return "H";
-	}
-	if (prochain_y < robot_y) {
-		astar_weight[robot_y - 2][robot_x - 1]++;
-		return "B";
-	}
-	
-	document.getElementById('debug').innerHTML =
-		'robot_x = '+robot_x+', robot_y = ' + robot_y + '<br />'
-		 + 'CH.voit = '+CH.voit+', CH.dist = '+CH.dist + '<br />'
-		 +' CB.voit = '+CB.voit+', CB.dist = '+CB.dist + '<br />'
-		 +' CG.voit = '+CG.voit+', CG.dist = '+CG.dist + '<br />'
-		 +' CD.voit = '+CD.voit+', CD.dist = '+CD.dist + '<br />'
-		 + ' but_dir_hb = ' + but_dir_hb + ', but_dir_gd = ' + but_dir_gd + '<br />'
-		 + ' but_dist = ' + but_dist + ', reserve_carburant = ' + reserve_carburant;
+    debug_pcc();
+    debug_pcc_goal();
 
-	return reserve_carburant % 2 == 0 ? "D" : "H";
+    // But visible et accessible => go
+    if (CH.voit==2 && CH.dist <= reserve_carburant) { return 'H'; }
+    if (CB.voit==2 && CB.dist <= reserve_carburant) { return 'B'; }
+    if (CG.voit==2 && CG.dist <= reserve_carburant) { return 'G'; }
+    if (CD.voit==2 && CD.dist <= reserve_carburant) { return 'D'; }
+
+
+    var pcc_robot = pcc[robot_y][robot_x];
+    if (reserve_carburant < 30 && pcc_robot >= reserve_carburant - 1) {
+        if (typeof pcc[robot_y + 1] != 'undefined' && pcc[robot_y + 1][robot_x] < pcc_robot) {
+            return "H";
+        }
+        if (typeof pcc[robot_y - 1] != 'undefined' && pcc[robot_y - 1][robot_x] < pcc_robot) {
+            return "B";
+        }
+        if (typeof pcc[robot_y][robot_x - 1] != 'undefined' && pcc[robot_y][robot_x - 1] < pcc_robot) {
+            return "G";
+        }
+        if (typeof pcc[robot_y][robot_x + 1] != 'undefined' && pcc[robot_y][robot_x + 1] < pcc_robot) {
+            return "D";
+        }
+    }
+
+    // Une seule direction possible...
+    if (CD.dist + CB.dist + CG.dist == 3) { return 'H'; }
+    if (CH.dist + CG.dist + CD.dist == 3) { return 'B'; }
+    if (CH.dist + CD.dist + CB.dist == 3) { return 'G'; }
+    if (CH.dist + CG.dist + CB.dist == 3) { return 'D'; }
+
+    var chemin = chemin_astar();
+    var prochain_x = chemin[0].y + 1;
+    var prochain_y = chemin[0].x + 1;
+
+    debug_astar(chemin);
+
+    if (prochain_x > robot_x) {
+        astar_weight[robot_y - 1][robot_x]++;
+        return "D";
+    }
+    if (prochain_x < robot_x) {
+        astar_weight[robot_y - 1][robot_x - 2]++;
+        return "G";
+    }
+    if (prochain_y > robot_y) {
+        astar_weight[robot_y][robot_x - 1]++;
+        return "H";
+    }
+    if (prochain_y < robot_y) {
+        astar_weight[robot_y - 2][robot_x - 1]++;
+        return "B";
+    }
+
+    document.getElementById('debug').innerHTML =
+        'robot_x = '+robot_x+', robot_y = ' + robot_y + '<br />'
+        + 'CH.voit = '+CH.voit+', CH.dist = '+CH.dist + '<br />'
+        +' CB.voit = '+CB.voit+', CB.dist = '+CB.dist + '<br />'
+        +' CG.voit = '+CG.voit+', CG.dist = '+CG.dist + '<br />'
+        +' CD.voit = '+CD.voit+', CD.dist = '+CD.dist + '<br />'
+        + ' but_dir_hb = ' + but_dir_hb + ', but_dir_gd = ' + but_dir_gd + '<br />'
+        + ' but_dist = ' + but_dist + ', reserve_carburant = ' + reserve_carburant;
+
+    return reserve_carburant % 2 == 0 ? "D" : "H";
 }
